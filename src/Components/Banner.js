0a9e5f311612ecd6f5557e23ca2a0f68d24eda38
P4CKt <omurcanuslu@outlook.com>
2023-02-14 02:03:53 +0300

    orderlist ekledim

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link,Route,Switch } from "react-router-dom";
+import { Link,Route,Switch,BrowserRouter } from "react-router-dom";
 
 const Banner = () => {
 
@@ -11,19 +11,9 @@ const Banner = () => {
 
 
   return (
-   
-      <div className="main-banner">
-        <div className="pizza">
-          
-          <img className="img-banner" src="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"/> 
-          <Link to="/pizza" id="order-pizza">Pizza ?</Link>
-        </div>
-
-          </div>
-    
-   
-    
-   
+     <div>
+        <p>affasad</p>
+     </div> 
   );
 };
 export default Banner;
