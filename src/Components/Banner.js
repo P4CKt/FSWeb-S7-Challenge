a83632e0879a4bff237afac4b34fb907bc94ea56
P4CKt <omurcanuslu@outlook.com>
2023-02-14 02:07:51 +0300

    banner geldi

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -11,9 +11,15 @@ const Banner = () => {
 
 
   return (
-     <div>
-        <p>affasad</p>
-     </div> 
+   <div className="main-banner">
+   <div className="pizza">
+
+     <img className="img-banner" src="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"/> 
+     <Link to="/pizza" id="order-pizza">Pizza ?</Link>
+   </div>
+
+     </div>
+
   );
 };
 export default Banner;
